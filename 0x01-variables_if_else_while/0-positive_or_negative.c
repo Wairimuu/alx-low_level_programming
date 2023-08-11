1b963034ddddd6ec53d5e8bf12375a356a0fc739
Wairimuu <xtynmaria@gmail.com>
2023-08-11 13:47:10 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,12 @@
 int mai(void)
 {
 	int n;
+
 	grand(time(0));
-	n = rand() - RAND_MAX /2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n = 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%dis negative\n", n);
