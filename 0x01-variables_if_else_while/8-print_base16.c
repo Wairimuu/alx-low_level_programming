129e58fb3379b7b5b2702a50b8244781316329b0
Wairimuu <xtynmaria@gmail.com>
2023-08-14 14:53:21 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 int i;
 char j;
 for (i = 0 ; i < 10 ; i++)
-        putchar(i + '0');
+putchar(i + '0');
 for (j = 'a' ; j <= 'f' ; j++)
-        putchar(j);
+putchar(j);
 putchar('\n');
 return (0);
 }
