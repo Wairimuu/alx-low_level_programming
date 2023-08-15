9365b6ecee2a234254948cb3360c2e8d0aeefe68
Wairimuu <xtynmaria@gmail.com>
2023-08-15 10:12:20 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table(void);
+void print_to_98(int n);
+void print_times_table(int n);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int, int);
+int print_last_digit(int);
+
+#endif
