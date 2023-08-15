d6e0cae79d647f89908ee4bf65a47ea9b40b6638
Wairimuu <xtynmaria@gmail.com>
2023-08-14 17:10:20 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,26 +6,26 @@
  */
 int main(void)
 {
-        int i, j, k;
-        for (i = 0; i < 10; i++)
-        {
-                for (j = i + 1; j < 10; j++)
-                {
-                        for (k = j + 1; k < 10; k++)
-                        {
-                                if (i < j && j < k)
-                                {
-                                        putchar(i + 48);
-                                        putchar(j + 48);
-                                        putchar(k + 48);
-                                        if (i == 7 && k == 9)
-                                                continue;
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+if (i < j && j < k)
+{
+putchar(i + 48);
+putchar(j + 48);
+putchar(k + 48);
+if (i == 7 && k == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
